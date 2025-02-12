07a5b74ed75909d706125b7ff8ce54568864f7c8
miku03090831 <1965489500@qq.com>
2025-02-13 00:07:39 +0800

    add type definition

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,28 +2,54 @@ import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "./src/index.ts",
-  jsx: "preserve",
-  output: [
-    {
-      dir: "dist/esm",
-      format: "esm",
-      preserveModules: true,
-    },
-    {
-      dir: "dist/cjs",
-      format: "cjs",
-      preserveModules: true,
-    },
-  ],
-  plugins: [
-    resolve(),
-    babel({ babelHelpers: "runtime" }),
-    commonjs(),
-    typescript(),
-  ],
-  external: ["react", "react-dom"],
-};
-
+export default [
+  {
+    input: "./src/index.ts",
+    jsx: "preserve",
+    output: [
+      {
+        dir: "dist/esm",
+        format: "esm",
+        preserveModules: true,
+      },
+      {
+        dir: "dist/cjs",
+        format: "cjs",
+        preserveModules: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      babel({ babelHelpers: "runtime" }),
+      commonjs(),
+      typescript(),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "./src/index.ts",
+    jsx: "preserve",
+    output: [
+      {
+        dir: "dist/esm",
+        format: "esm",
+        preserveModules: true,
+      },
+      {
+        dir: "dist/cjs",
+        format: "cjs",
+        preserveModules: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      babel({ babelHelpers: "runtime" }),
+      commonjs(),
+      typescript(),
+      dts(),
+    ],
+    external: ["react", "react-dom"],
+  },
+];
