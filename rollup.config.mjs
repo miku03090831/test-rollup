56d8b1ca1ec95527d70e71bcf7fcfccdea0ae3a2
miku03090831 <1965489500@qq.com>
2025-03-18 00:55:16 +0800

    delete useless configs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -15,15 +15,7 @@ export default [
       {
         dir: "dist/esm",
         format: "esm",
-        preserveModules: true,
-        assetFileNames: ({ name }) => {
-          console.log("111", path.parse(name));
-          const { ext, dir, base } = path.parse(name);
-          if (ext !== ".css") {
-            return "[name].[ext]";
-          }
-          return path.join(dir, "style", base);
-        },
+        preserveModules: true
       },
       {
         dir: "dist/cjs",
@@ -36,7 +28,7 @@ export default [
       babel({ babelHelpers: "runtime" }),
       commonjs(),
       typescript(),
-      styles({ mode: "extract", sourceMap: true }),
+      styles({ mode: "extract", sourceMap: true}),
     ],
     external: ["react", "react-dom"],
   },
