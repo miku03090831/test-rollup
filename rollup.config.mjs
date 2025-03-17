529f2d4bfc68e7730f0cf557c3fd86729d81194a
miku03090831 <1965489500@qq.com>
2025-03-18 00:17:18 +0800

    use another css plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
+import styles from "rollup-plugin-styles";
+import path from "path";
 
 export default [
   {
@@ -14,6 +16,14 @@ export default [
         dir: "dist/esm",
         format: "esm",
         preserveModules: true,
+        assetFileNames: ({ name }) => {
+          console.log("111", path.parse(name));
+          const { ext, dir, base } = path.parse(name);
+          if (ext !== ".css") {
+            return "[name].[ext]";
+          }
+          return path.join(dir, "style", base);
+        },
       },
       {
         dir: "dist/cjs",
@@ -26,33 +36,33 @@ export default [
       babel({ babelHelpers: "runtime" }),
       commonjs(),
       typescript(),
-      postcss({ extract: true }),
+      styles({ mode: "extract", sourceMap: true }),
     ],
     external: ["react", "react-dom"],
   },
-  {
-    input: "./src/index.ts",
-    jsx: "preserve",
-    output: [
-      {
-        dir: "dist/esm",
-        format: "esm",
-        preserveModules: true,
-      },
-      {
-        dir: "dist/cjs",
-        format: "cjs",
-        preserveModules: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      babel({ babelHelpers: "runtime" }),
-      commonjs(),
-      typescript(),
-      postcss({ extract: true }),
-      dts(),
-    ],
-    external: ["react", "react-dom"],
-  },
+  // {
+  //   input: "./src/index.ts",
+  //   jsx: "preserve",
+  //   output: [
+  //     {
+  //       dir: "dist/esm",
+  //       format: "esm",
+  //       preserveModules: true,
+  //     },
+  //     {
+  //       dir: "dist/cjs",
+  //       format: "cjs",
+  //       preserveModules: true,
+  //     },
+  //   ],
+  //   plugins: [
+  //     resolve(),
+  //     babel({ babelHelpers: "runtime" }),
+  //     commonjs(),
+  //     typescript(),
+  //     postcss({ extract: true }),
+  //     dts(),
+  //   ],
+  //   external: ["react", "react-dom"],
+  // },
 ];
